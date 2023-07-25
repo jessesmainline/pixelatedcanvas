d3e2b4d3d9851c03da6eb5c6362f92e32bd03262
jesseml <jessesmainline@gmail.com>
2023-07-25 11:38:48 -0700

    buttons to add and square bug to fix

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const gridContainer = document.getElementById("gridContainer");
 const resetButton = document.getElementById("resetButton");
 
+function getRandomRGBValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+function darkenRGBValue(value, percentage) {
+    return Math.floor(value * (1 - percentage));
+}
+
+function changeSquareColor(square) {
+    const currentColor = window.getComputedStyle(square).backgroundColor;
+    const match = currentColor.match(/(\d+),\s*(\d+),\s*(\d+)/);
+
+    if (match) {
+        const r = parseInt(match[1]);
+        const g = parseInt(match[2]);
+        const b = parseInt(match[3]);
+
+        const newR = getRandomRGBValue();
+        const newG = getRandomRGBValue();
+        const newB = getRandomRGBValue();
+
+        const darkenPercentage = 0.1;
+        const newColor = `rgb(${darkenRGBValue(newR, darkenPercentage)}, ${darkenRGBValue(newG, darkenPercentage)}, ${darkenRGBValue(newB, darkenPercentage)})`;
+
+        square.style.backgroundColor = newColor;
+    }
+}
+
 function createGrid(size) {
     gridContainer.innerHTML = "";
     gridContainer.style.setProperty("--grid-size", size);
@@ -8,6 +36,7 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseover", () => changeSquareColor(square));
         gridContainer.appendChild(square);
     }
 }
